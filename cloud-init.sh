41f283b2d526c957a42d82f1aedee94b5dc75614
scott coulton <scott.coulton@microsoft.com>
2021-07-29 10:16:56 +1000

    added bootstrap

--- cloud-init.sh
+++ cloud-init.sh
@@ -14,10 +14,7 @@ sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 sudo microk8s config > $HOME/.kube/config
 echo "--enable-bootstrap-token-auth" > /var/snap/microk8s/current/args/kube-apiserver
-systemctl restart snap.microk8s.daemon-apiserver
-curl -o bootstrap.sh 'https://raw.githubusercontent.com/krustlet/krustlet/main/scripts/bootstrap.sh'
-chmod +x bootstrap.sh
-./bootstrap.sh
+
 
 
 tee -a ~/.bashrc <<'EOF'
@@ -31,6 +28,10 @@ echo " # Krustlet..."
 VERSION="v1.0.0-alpha.1"
 wget https://krustlet.blob.core.windows.net/releases/krustlet-$VERSION-linux-amd64.tar.gz
 tar -C /usr/local/bin -xzf krustlet-$VERSION-linux-amd64.tar.gz
+systemctl restart snap.microk8s.daemon-apiserver
+curl -o bootstrap.sh 'https://raw.githubusercontent.com/krustlet/krustlet/main/scripts/bootstrap.sh'
+chmod +x bootstrap.sh
+./bootstrap.sh
 
 echo " # krustlet config..."
 ./KUBECONFIG=${PWD}/krustlet-config \
