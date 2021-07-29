571a7dd1f3d558862520770bea5a08f2e672aee9
scott coulton <scott.coulton@microsoft.com>
2021-07-29 10:18:59 +1000

    fixed ordering

--- cloud-init.sh
+++ cloud-init.sh
@@ -14,9 +14,7 @@ sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 sudo microk8s config > $HOME/.kube/config
 echo "--enable-bootstrap-token-auth" > /var/snap/microk8s/current/args/kube-apiserver
-
-
-
+systemctl restart snap.microk8s.daemon-apiserver
 tee -a ~/.bashrc <<'EOF'
 function kubectl {
         sudo microk8s kubectl "$@"
@@ -28,7 +26,6 @@ echo " # Krustlet..."
 VERSION="v1.0.0-alpha.1"
 wget https://krustlet.blob.core.windows.net/releases/krustlet-$VERSION-linux-amd64.tar.gz
 tar -C /usr/local/bin -xzf krustlet-$VERSION-linux-amd64.tar.gz
-systemctl restart snap.microk8s.daemon-apiserver
 curl -o bootstrap.sh 'https://raw.githubusercontent.com/krustlet/krustlet/main/scripts/bootstrap.sh'
 chmod +x bootstrap.sh
 ./bootstrap.sh
